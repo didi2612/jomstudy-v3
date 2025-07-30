cfb03eb8a63c9178106b049ee38afc94e0116466
didi2612 <github.azmiproductions.com>
2025-07-30 19:56:25 +0800

    modified:   src/pages/Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -88,7 +88,7 @@ export default function Home() {
           <img
             src="https://i.pinimg.com/originals/57/3c/da/573cdaf5205bebaac51ca29273dd5514.gif"
             alt="Study girl"
-            className="relative w-full rounded-3xl shadow-xl border border-white/10"
+            className="relative w-full rounded-3xl shadow-xl  "
           />
         </motion.div>
       </section>
